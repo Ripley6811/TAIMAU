1aefa7baf61dcf5ac86166a5a39dc0503c0fe794
Jay W Johnson <tastethejava@hotmail.com>
2014-08-29 00:56:07 +0800

    Database Migration
    Finished script for migrating the database from version 2 to version 3

--- src/db_tools/TM2014_tables_v3.py
+++ src/db_tools/TM2014_tables_v3.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+"""
+Version 3 splits Order into Order and OrderItem,
+Shipment into Shipment and ShipmentItem.
+Just as Invoice was split from version 1 to 2.
+InvoiceItem and ShipmentItem now reference each other.
+Invoice number is removed as primary key and placed with integer.
+
+NOTE: Class functions can be changed and added without migration.
+
+"""
+
 import sqlalchemy as sqla
 from sqlalchemy.orm import relationship as rel
 from sqlalchemy.orm import backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
-import json
 
 Int = sqla.Integer
-Str = sqla.String  #TODO: Can probably delete this line
+#Str = sqla.String  #TODO: Can probably delete this line
 Utf = sqla.Unicode
 Float = sqla.Float
 Col = sqla.Column
@@ -51,14 +62,6 @@ def AddDictRepr(aClass):
 #==============================================================================
 # Order class
 #==============================================================================
-def updatetotal(context):
-    subtotal = context.current_parameters['subtotal']
-    taxed = context.current_parameters['applytax']
-    if taxed:
-        return int(round(subtotal * 1.05))
-    else:
-        return int(round(subtotal))
-
 @AddDictRepr
 class Order(Base):
     __tablename__ = 'order'
@@ -75,14 +78,17 @@ class Order(Base):
 
     price = Col(Float) # Price for one SKU or unit on this order
     discount = Col(Int) # Discount percentage as integer (0-100)
-    totalskus = Col(Int)
-    totalunits = Col(Float) # AUTO: unitssku * totalskus
-    subtotal = Col(Float) #TODO: Remove and leave final totals in attached invoice.
+    #XXX: name change in version 3: totalskus = Col(Int)
+    qty = Col(Int)
     applytax = Col(Bool) # True = 5%, False = 0%
-    totalcharge = Col(Int, default=updatetotal, onupdate=updatetotal) #TODO: Remove
 
-    orderdate = Col(Date)  # Use as "expected" delivery date. (Originally was order placement date)
-    duedate = Col(Date, nullable=False)  # Expected delivery date
+    #XXX: Remove in version 3
+    #totalunits = Col(Float) # AUTO: unitssku * totalskus
+    #subtotal = Col(Float) #TODO: Remove and leave final totals in attached invoice.
+    #totalcharge = Col(Int) #TODO: Remove
+
+    orderdate = Col(Date, nullable=False)  # Order placement date
+    duedate = Col(Date)  # Expected delivery date
     date = Col(Date) # Extra date field if needed
 
     orderID = Col(Utf)  # PO Number
@@ -91,19 +97,21 @@ class Order(Base):
     note = Col(Utf) # Extra note field if needed.
 
     checked = Col(Bool, nullable=False, default=False) # Match against second party records
-    is_sale = Col(Bool, nullable=False) # Boolean for purchase vs. sale. Also check against buyer/seller name.
+    is_sale = Col(Bool, nullable=False, default=False) # Boolean
+    is_purchase = Col(Bool, nullable=False, default=False) # Boolean
 
-    shipments = rel('Shipment', backref='order')
+    shipments = rel('ShipmentItem', backref='order')
     invoices = rel('InvoiceItem', backref='order')
     product = rel('Product')
 
-    #ADD: isOpen = Col(Bool, default=True) # Active or closed PO
+    #XXX: New in version 3
+    is_open = Col(Bool, default=True) # Active or closed PO
 
     def qty_shipped(self):
         '''By number of SKUs'''
         if len(self.shipments) == 0:
             return 0
-        return sum([srec.sku_qty if isinstance(srec.sku_qty,int) else 0 for srec in self.shipments])
+        return sum([srec.qty if isinstance(srec.qty,int) else 0 for srec in self.shipments])
 
     def qty_remaining(self):
         '''By number of SKUs remaining to be shipped'''
@@ -119,7 +127,7 @@ class Order(Base):
         '''By number of SKUs'''
         if len(self.invoices) == 0:
             return 0
-        return sum([prec.sku_qty if isinstance(prec.sku_qty,int) else 0 for prec in self.invoices])
+        return sum([prec.qty if isinstance(prec.qty,int) else 0 for prec in self.invoices])
 
     def all_invoiced(self):
         '''By number of SKUs'''
@@ -130,7 +138,7 @@ class Order(Base):
     def total_paid(self):
         if len(self.invoices) == 0:
             return 0
-        return sum([prec.sku_qty if prec.invoice.paid else 0 for prec in self.invoices])
+        return sum([prec.qty if prec.paid else 0 for prec in self.invoices])
 
     def all_paid(self):
         if len(self.invoices) == 0:
@@ -138,18 +146,10 @@ class Order(Base):
         return not (False in [prec.invoice.paid for prec in self.invoices])
 
     def qty_quote(self, qty):
-        '''Subtotal of total order.'''
         subtotal = self.price * qty
         if self.product.unitpriced:
             subtotal *= self.product.units
         return int(round(subtotal))
-
-#    def formatted(self):
-#        txt =  u'PO# {s.orderID:<16}  {s.seller:>8} >> {s.buyer:<8}'
-#        txt += u'  {s.totalskus:>5} {s.product.product_label} '
-#
-#        return txt.format(s=self)
-
 
     def listbox_summary(self):
         """
@@ -181,7 +181,7 @@ class Order(Base):
 
 
         try:
-            tmp += u"{0:%y}年{0.month:>2}月{0.day:>2}日".format(self.orderdate)
+            tmp += u"訂單日:{0.month:>2}月{0.day:>2}日".format(self.orderdate)
         except:
             tmp += u'(Order date not entered)'
 
@@ -194,7 +194,7 @@ class Order(Base):
             tmp += u"\u273F {rem_qty:>6}{s.totalskus:>5}{um} {pt:<14} @ ${pr} \u214C {um}".format(
                 pt= prodtmp,
                 pr= int(self.price) if float(self.price).is_integer() else self.price,
-                um= self.product.SKU if self.product.SKU != u'槽車' else self.product.UM,
+                um= self.product.UM if self.product.unitpriced else self.product.SKU,
                 rem_qty= u'{}/'.format(self.qty_remaining()),
                 s= self,
                 )
@@ -215,30 +215,47 @@ class Shipment(Base): # Keep track of shipments/SKUs for one order
     '''
     __tablename__ = 'shipment'
     id = Col(Int, primary_key=True)
-    order_id = Col(Int, ForeignKey('order.id'), nullable=False)
-
-    sku_qty = Col(Int, nullable=False) # Deduct from total SKUs due
+    #XXX: Removed in version 3
+    #order_id = Col(Int, ForeignKey('order.id'), nullable=False)
+    #qty = Col(Int, nullable=False) # Deduct from total SKUs due
 
     shipmentdate = Col(Date, nullable=False)
-    shipmentID = Col(Utf, default=u'') # i.e., Manifest number
+    shipment_no = Col(Utf, default=u'') # i.e., Manifest number
     shipmentnote = Col(Utf, default=u'') # Information concerning the delivery
-    #XXX: shipmentdest = Col(Utf, default=u'')
+    #XXX: New in version 3
+    shipmentdest = Col(Utf, default=u'')
 
-    #TODO: Add destination or add to note (maybe JSON)
     driver = Col(Utf) # Track vehicle driver (optional)
     truck = Col(Utf) # Track vehicle by license (optional)
     note = Col(Utf) # Extra note field if needed
     checked = Col(Bool, nullable=False, default=False) # Extra boolean for matching/verifying
+
+    #XXX: New in version 3
+    items = rel('ShipmentItem', backref='shipment')
 
     # METHODS
     def listbox_summary(self):
         """
         Return a single line unicode summary intended for a listbox.
         """
-        txt = u'{date:<10}   編號: {s.shipmentID:<10}   QTY: {s.sku_qty:>5} {s.order.product.SKU:<6}   品名: {s.order.product.inventory_name}'
+        txt = u'{date:<10}   編號: {s.shipment_no:<10}   QTY: {s.items[0].qty:>5} {s.order.product.SKU:<6}   品名: {s.order.product.inventory_name}'
         txt = txt.format(s=self, date=str(self.shipmentdate))
         return txt
 
+@AddDictRepr
+class ShipmentItem(Base):
+    __tablename__ = 'shipmentitem'
+    id = Col(Int, primary_key=True)
+
+    order_id = Col(Int, ForeignKey('order.id'), nullable=False)
+    shipment_id = Col(Int, ForeignKey('shipment.id'), nullable=False)
+
+    qty = Col(Int, nullable=False) # Deduct from total SKUs due
+
+    lot_start = Col(Int)
+    lot_end = Col(Int)
+
+    invoiceitem = rel('InvoiceItem', backref='shipmentitem')
 
 #==============================================================================
 # Invoice class (track multiple invoices for one order)
@@ -246,8 +263,10 @@ class Shipment(Base): # Keep track of shipments/SKUs for one order
 @AddDictRepr
 class Invoice(Base): # Keep track of invoices/payments for one order
     __tablename__ = 'invoice'
-    invoice_no = Col(Utf, primary_key=True) # i.e., Invoice number
-    #id = Col(Int, primary_key=True)
+    #XXX: New in version 3, primary key change
+    id = Col(Int, primary_key=True)
+
+    invoice_no = Col(Utf, default=u'') # i.e., Invoice number
 
     seller = Col(Utf, ForeignKey('branch.name'), nullable=False) # For billing/receipts
     buyer = Col(Utf, ForeignKey('branch.name'), nullable=False) # For billing/receipts
@@ -284,21 +303,21 @@ class InvoiceItem(Base): # Keep track of invoices/payments for one order
     '''
     order : backref to Order
     invoice : backref to Invoice
-
-    #TODO: Add ref from InvoiceItem to ShipmentItem, one-to-one
+    shipmentitem : backref to ShipmentItem
     '''
     __tablename__ = 'invoiceitem'
     id = Col(Int, primary_key=True)
 
-    invoice_no = Col(Utf, ForeignKey('invoice.invoice_no'), nullable=False)
+    invoice_id = Col(Utf, ForeignKey('invoice.id'), nullable=False)
+    shipmentitem_id = Col(Utf, ForeignKey('shipmentitem.id'), nullable=False)
 
     order_id = Col(Int, ForeignKey('order.id'), nullable=False)
 
-    sku_qty = Col(Int, nullable=False)
+    qty = Col(Int, nullable=False)
 
 
     def subtotal(self):
-        subtotal = self.order.price * self.sku_qty
+        subtotal = self.order.price * self.qty
         if self.order.product.unitpriced:
             subtotal *= self.order.product.units
         return int(round(subtotal))
@@ -307,7 +326,7 @@ class InvoiceItem(Base): # Keep track of invoices/payments for one order
         """
         Return a single line unicode summary intended for a listbox.
         """
-        txt = u'{date:<10}   編號: {s.invoice_no:<10}   QTY: {s.sku_qty:>5} {s.order.product.SKU:<6}   Subtotal: ${total:<6}   品名: {s.order.product.inventory_name}'
+        txt = u'{date:<10}   編號: {s.invoice_no:<10}   QTY: {s.qty:>5} {s.order.product.SKU:<6}   Subtotal: ${total:<6}   品名: {s.order.product.inventory_name}'
         txt = txt.format(s=self, date=str(self.invoice.invoicedate), total=self.subtotal())
         return txt
 
@@ -318,15 +337,13 @@ class InvoiceItem(Base): # Keep track of invoices/payments for one order
 #==============================================================================
 @AddDictRepr
 class CoGroup(Base):
-    # Primary key is abbreviated "name" of group.
-    #
     __tablename__ = 'cogroup'
     name = Col(Utf, primary_key=True, nullable=False) # Abbreviated name of company (2 to 4 chars)
     is_active = Col(Bool, nullable=False, default=True) # Boolean for listing the company. Continuing business.
     is_supplier = Col(Bool, nullable=False, default=True) # Maybe use in later versions
     is_customer = Col(Bool, nullable=False, default=True) # Maybe use in later versions
 
-    branches = rel('Branch', lazy='joined')
+    branches = rel('Branch', lazy='joined') # lazy -> Attaches on retrieving a cogroup
     orders = rel('Order')
     products = rel('Product')
     contacts = rel('Contact')
@@ -342,8 +359,6 @@ class CoGroup(Base):
 #==============================================================================
 @AddDictRepr
 class Branch(Base):
-    # Primary key is abbreviated "name" of branch company.
-    #
     __tablename__ = 'branch'
     name = Col(Utf, primary_key=True, nullable=False) # Abbreviated name of company (2 to 4 chars)
     group= Col(Utf, ForeignKey('cogroup.name'), nullable=False) # Name of main company representing all branches
@@ -424,7 +439,9 @@ class Product(Base): # Information for each unique product (including packaging)
     SKU = Col(Utf, nullable=False)  #Stock keeping unit (sold unit)
     SKUlong = Col(Utf, default=u'')
     unitpriced = Col(Bool, nullable=False)
-    ASE_PN = Col(Utf)
+    ASE_PN = Col(Utf) # ASE product number
+    ASE_RT = Col(Utf) # ASE department routing number
+    ASE_END = Col(Int) # Last used SKU index number for current lot
     note = Col(Utf) # {JSON} contains extra data, i.e. current ASE and RT numbers
                     # {JSON} must be appended to the end after any notes. Last char == '}'
     is_supply = Col(Bool, nullable=False)
@@ -432,7 +449,7 @@ class Product(Base): # Information for each unique product (including packaging)
 
     curr_price = Col(Float, default=0.0)
 
-    summary = Col(Utf)
+    #summary = Col(Utf, default=summarize, onupdate=summarize)
 
     stock = rel('Stock', backref='product')
     orders = rel('Order', primaryjoin="Product.MPN==Order.MPN")
@@ -466,51 +483,18 @@ class Product(Base): # Information for each unique product (including packaging)
         else:
             return self.inventory_name
 
-    def SKUlabel(self):
-        if self.unitpriced or self.SKU == u'槽車':
-            return self.UM
-        else:
-            return self.SKU
-
-    def json(self, new_dic=None):
-        '''Saves 'new_dic' as a json string and overwrites previous json.
-        Returns contents of json string as a dictionary object.
-        Note: Commit session after making changes.'''
-        if new_dic == None:
-            if self.note.find(u'}') != -1:
-                return json.loads(self.note[self.note.index(u'{'):])
-            else:
-                return None
-        else:
-            old_dic = dict()
-            # Delete existing json string
-            if self.note.find(u'}') != -1:
-                old_dic = self.json()
-                self.note = self.note.split(u'{')[0]
-            # Merge and overwrite old with new.
-            new_dic = dict(old_dic.items() + new_dic.items())
-            self.note += json.dumps(new_dic)
-            return True
-        return False
-
-    def summarize(self):
+    def specs(self):
         '''Short text of product key values.
-        "product_name (## UM SKU)"
-        e.g. "HCL (20 kg barrel)"
+        "## UM SKU"
+        e.g. "20 kg barrel"
         '''
-        outname = self.product_label
-        units = float(self.units)
-        units = int(units) if int(units)==units else units #Truncate if mantissa is zero
-        UM = self.UM
-        SKU = self.SKU
-        if not outname:
-            outname = self.inventory_name
-        if SKU == u'槽車':
-            return u"{0} (槽車)".format(outname)
+        u = self.units
+        units = int(u) if int(u)==u else u #Truncate if mantissa is zero
+        if self.SKU == u'槽車':
+            return u'槽車-{}'.format(self.UM)
         else:
-            units = int(units) if int(units)==units else units
-            uf = u"{0} ({1} {2} {3})"
-            return uf.format(outname,units,UM,SKU)
+            txt = u"{0} {1} {2}"
+            return txt.format(units,self.UM,self.SKU)
 
 
 
@@ -570,7 +554,7 @@ if __name__ == '__main__':
     order = Order(orderID=u'ZV1234', seller=u'Oscorp', buyer=u'Marvel', group=u'DC comics',
                   is_sale=True, MPN=666, subtotal=1000, duedate=datetime.date.today(), totalskus=24)
     product = Product(MPN=666, group=u'DC comics', units=12, UM=u'ounce', SKU=u'vial', unitpriced=False, is_supply=False, product_label=u'Muscle Juice', inventory_name=u'serim 234')
-    shipment = Shipment(shipmentID=u'003568', sku_qty=10, order=order, shipmentdate=datetime.date.today())
+    shipment = Shipment(shipment_no=u'003568', qty=10, order=order, shipmentdate=datetime.date.today())
     session.add(product)
     session.add(order)
     order = session.query(Order).first()
---
+++ src/db_tools/migrate_db_v2_v3.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sqlalchemy.orm import sessionmaker
+import db_manager as DM # v2 manager
+import datetime
+import random
+from TM2014_tables_v3 import get_database, CoGroup, Branch, Product, Contact, Stock, Order, Shipment, ShipmentItem, Invoice, InvoiceItem, Vehicle
+
+
+dbname = r"C:\Users\Jay\Dropbox\GitHub\TAIMAU\src\TM2014_v3.db"
+database = get_database(dbname)
+newsession = sessionmaker(bind=database)()
+
+#TODO: Still needs testing in the program to make sure all records appear normal
+
+#===============================================================================
+# MIGRATE COMPANIES AND PRODUCTS AND ASSETS
+
+added = set()
+for i, rec in enumerate(DM.session.query(DM.CoGroup)):
+    rec = rec.__dict__
+    if i==0:
+        print "CoGroup -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+    del rec['branches']
+#    print ">", rec
+
+    newsession.add(CoGroup(**rec))
+
+for i, rec in enumerate(DM.session.query(DM.Branch)):
+    rec = rec.__dict__
+    if i==0:
+        print "Branch -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+#    print ">", rec
+
+    newsession.add(Branch(**rec))
+
+for i, rec in enumerate(DM.session.query(DM.Contact)):
+    rec = rec.__dict__
+    if i==0:
+        print "Contact -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+#    print ">", rec
+
+    newsession.add(Contact(**rec))
+
+for i, rec in enumerate(DM.session.query(DM.Product)):
+    rec = rec.__dict__
+    if i==0:
+        print "Product -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+    del rec['summary']
+#    print ">", rec
+
+    newsession.add(Product(**rec))
+
+for i, rec in enumerate(DM.session.query(DM.Stock)):
+    rec = rec.__dict__
+    if i==0:
+        print "Stock -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+#    print ">", rec
+
+    newsession.add(Stock(**rec))
+
+for i, rec in enumerate(DM.session.query(DM.Vehicle)):
+    rec = rec.__dict__
+    if i==0:
+        print "Vehicle -----------------------------------------"
+        for each in rec.keys():
+            print each
+    del rec['_sa_instance_state']
+#    print ">", rec
+
+    newsession.add(Vehicle(**rec))
+
+
+#==============================================================================
+# ORDERS, SHIPMENTS AND INVOICES
+#==============================================================================
+notmatched = 0
+for i, rec in enumerate(DM.session.query(DM.Order)):
+    # Ref to shipments and invoices for processing later
+    Shipments = rec.shipments
+    InvoiceItems = rec.invoices
+
+    shipped = rec.all_shipped()
+    invoiced = rec.all_invoiced()
+#    print shipped, invoiced
+    rec = rec.__dict__
+    if i==0:
+        print "Order -----------------------------------------"
+        for each in rec.keys():
+            print each
+
+    rec['is_open'] = not(shipped & invoiced)
+    rec['is_purchase'] = not rec['is_sale']
+    rec['qty'] = rec.pop('totalskus') # Rename
+
+    del rec['_sa_instance_state']
+    del rec['totalcharge']
+    del rec['subtotal']
+    del rec['totalunits']
+    del rec['shipments']
+    del rec['invoices']
+#    print ">", rec
+
+    newsession.add(Order(**rec))
+    if i==0:
+        print "New Order -----------------------------------------"
+        for each in rec.keys():
+            print each
+    print '  ======================================='
+    print '  Order:', rec['id'], ' Shipments:', len(Shipments), ' Invoices:', len(InvoiceItems)
+    ### PROCESS SHIPMENTS
+    nInv = len(InvoiceItems)
+    nInvMatch = 0
+    for i, rec in enumerate(Shipments):
+        rec = rec.__dict__
+        recitem = dict()
+
+
+#        recitem['shipment_id'] = rec['id']
+        recitem['order_id'] = rec.pop('order_id')
+        recitem['qty'] = rec.pop('sku_qty')
+        del rec['_sa_instance_state']
+        rec['shipment_no'] = rec.pop('shipmentID')
+        if rec['shipment_no'] == u'N/A':
+            rec['shipment_no'] = u''
+        recitem['shipment'] = Shipment(**rec)
+
+        # Don't add multiple Shipment manifests
+        if nInvMatch < len(InvoiceItems):
+            for recinvitem in InvoiceItems:
+                recinv = recinvitem.invoice.__dict__
+                recinvitem = recinvitem.__dict__
+
+                if 'qty' in recinvitem.keys():
+                    continue
+                if recitem['qty'] == recinvitem['sku_qty']:
+
+        #            print i, "qty:", recitem['qty'], recinvitem['sku_qty'], recitem['qty'] == recinvitem['sku_qty']
+#                    if recitem['qty'] != recinvitem['sku_qty']:
+#                        notmatched += 1
+                    print i, "qty:", recitem['qty'], recinvitem['sku_qty'], recitem['qty'] == recinvitem['sku_qty']
+                    del recinv['_sa_instance_state']
+                    del recinvitem['_sa_instance_state']
+                    recinvitem['qty'] = recinvitem.pop('sku_qty')
+                    del recinvitem['invoice_no']
+
+                    recinvitem['shipmentitem'] = ShipmentItem(**recitem)
+                    recinvitem['invoice'] = Invoice(**recinv)
+
+                    newsession.add(InvoiceItem(**recinvitem))
+                    nInvMatch += 1
+                    break
+            else:
+#                print "InvoiceItem match not found",
+#                print "                          STILL MORE" if nInvMatch < len(InvoiceItems) else "DONE"
+                newsession.add(ShipmentItem(**recitem))
+        else:
+#            print "No InvoiceItems available"
+            newsession.add(ShipmentItem(**recitem))
+    print " ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !" if nInvMatch < len(InvoiceItems) else ""
+
+print "NOT MATCHED =", notmatched
+
+#==============================================================================
+# CONSOLIDATE SHIPMENT AND INVOICE, MATCH BY NUMBERS AND REDUCE
+#==============================================================================
+
+print newsession.query(Shipment).count(), "B Manifests"
+print newsession.query(ShipmentItem).count(), "B Manifest items"
+count = 0
+for shipment in newsession.query(Shipment).all():
+    if len(shipment.items) == 0:
+        count += 1
+print "Empty manifests:", count
+for shipment in newsession.query(Shipment).all():
+    if shipment.shipment_no:
+#        print shipment.shipment_no,
+        shipments = newsession.query(Shipment).filter(Shipment.shipment_no == shipment.shipment_no).all()
+        if 1 < len(shipments) < 6:
+            date = shipments[0].shipmentdate
+            nombre = shipments[0].shipment_no
+            _id = shipments[0].id
+
+            for ship in shipments[1:]:
+                if (date == ship.shipmentdate) & (nombre == ship.shipment_no):
+                    for shipitem in ship.items:
+#                        print shipitem.shipment.id, ">>>",
+                        shipitem.shipment = shipments[0]
+#                        print shipitem.shipment.id
+
+count = 0
+for shipment in newsession.query(Shipment).all():
+    if len(shipment.items) == 0:
+        newsession.delete(shipment)
+        count += 1
+print "Empty manifests:", count
+print newsession.query(Shipment).count(), "A Manifests"
+print newsession.query(ShipmentItem).count(), "A Manifest items"
+
+#srec = newsession.query(Shipment).get(7897)
+
+#print srec.__dict__
+#for sitems in srec.items:
+##    sitems.__dict__.pop('_sa_instance_state')
+#    try:
+#        sitems.__dict__.pop('invoiceitem')
+#    except:
+#        pass
+#    sitems.__dict__.pop('shipment')
+#    print sitems.__dict__
+#    orec = newsession.query(Order).get(sitems.order_id)
+#    print orec.product.MPN
+#    print sitems.order.product.MPN
+
+
+print "================================================================"
+### INVOICES
+print newsession.query(Invoice).count(), "B Invoices"
+print newsession.query(InvoiceItem).count(), "B Invoice items"
+count = 0
+for invoice in newsession.query(Invoice).all():
+    if len(invoice.items) == 0:
+        count += 1
+print "Empty manifests:", count
+for invoice in newsession.query(Invoice).all():
+    if invoice.invoice_no:
+#        print shipment.shipment_no,
+        invoices = newsession.query(Invoice).filter(Invoice.invoice_no == invoice.invoice_no).all()
+        if 1 < len(invoices) < 100:
+            date = invoices[0].invoicedate
+            nombre = invoices[0].invoice_no
+            _id = invoices[0].id
+
+            for inv in invoices[1:]:
+                # Merge invoice items if the invoice no and date are the same
+                if (date == inv.invoicedate) & (nombre == inv.invoice_no):
+                    for invitem in inv.items:
+                        print invitem.invoice.id, ">>>",
+                        invitem.invoice = invoices[0]
+                        print invitem.invoice.id
+
+count = 0
+for invoice in newsession.query(Invoice).all():
+    if len(invoice.items) == 0:
+        newsession.delete(invoice)
+        count += 1
+print "Empty manifests:", count
+print newsession.query(Invoice).count(), "A Invoices"
+print newsession.query(InvoiceItem).count(), "A Invoice items"
+
+
+
+#srec = newsession.query(Invoice).get(1115)
+#
+#print srec.__dict__
+#for sitems in srec.items:
+#    sitems.__dict__.pop('_sa_instance_state')
+#    try:
+#        sitems.__dict__.pop('shipmentitem')
+#    except:
+#        pass
+#    sitems.__dict__.pop('invoice')
+#    print sitems.__dict__
+#    orec = newsession.query(Order).get(sitems.order_id)
+#    print orec.product.MPN
+#    print sitems.order.product.MPN
+#TODO: Delete EMPTY Shipments and Invoices
+newsession.commit()
+print "++++++++++++++++++++++++++++++++++++"
+print "           DONE PROCESSING"
+print "++++++++++++++++++++++++++++++++++++"
+
+#    ### PROCESS INVOICES
+#    for i, rec in enumerate(Invoices):
+#        rec = rec.__dict__
+
+
+#for each in newsession.query(CoGroup).all():
+#    print [br.tax_id for br in each.branches]
+#
+#for each in newsession.query(CoGroup).all():
+#    print [br.english_name for br in each.products]
+
+#for each in newsession.query(Order).all():
+#    print each.id, "open" if each.is_open else "closed"
+#    print len(each.shipments), len(each.invoices)
+#print newsession.query(Order).count(), "Orders"
+
+
+
+# Ensure each invoice is matched once
+inv_matched_id = []
+
+
