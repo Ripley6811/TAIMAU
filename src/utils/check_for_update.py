91bb849adaf499d80683a4363de5d56c9aca994f
Jay W Johnson <jwj@boun.cr>
2014-12-07 16:34:11 +0800

    program update menu option added
    Program prompts the user and will download and copy newest github
    commit.

--- src/utils/check_for_update.py
+++ src/utils/check_for_update.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import Tix
+import os
+import shutil
 import tkMessageBox
-import datetime
 import urllib2
 from StringIO import StringIO
 from zipfile import ZipFile
+from requests import get as urlopen
 
 def update(_, settings):
     response = urllib2.urlopen("https://github.com/Ripley6811/TAIMAU")
     html = response.read()
-    prestring = u'commit/'
-    start = html.find(prestring) + len(prestring) + 1
-    end = html.find('" class', start)
+    prestring = 'commit/'
+    start = html.find(prestring) + len(prestring)
+    end = html.find('"', start)
 
     if _.debug:
         print start, end, html[start: end]
@@ -21,27 +22,55 @@ def update(_, settings):
     # Check if commit number is stored in settings.
     # Save and exit if it is not there.
     try:
-        print u"Latest commit SHA:"
-        print u"\t" + settings.load()[u'commit']
+        print u"Settings SHA:"
+        print u"\t" + settings.load()['commit']
     except KeyError:
         settings.update(commit = SHA)
         return
 
+    print u"Latest commit SHA:"
+    print "\t" + SHA
 #==============================================================================
 # Compare SHA in settings to web site latest
 #==============================================================================
-    if SHA == settings.load()[u'commit']:
+    if SHA == settings.load()['commit']:
         title = message = u'Program is up to date!'
         tkMessageBox.showinfo(title, message)
         return
     else:
         title = u'New version available!'
-        message = u'New version is available.\nUpdate now?'
+        message = u'Current:\n  ' + settings.load()['commit']
+        message += u'\nLatest:\n  ' + SHA
+        message += u'\n\nNew version is available.\nUpdate now?'
         confirm = tkMessageBox.askyesno(title, message)
         if not confirm:
             return
         else:
-            zipf = urllib2.urlopen("https://github.com/Ripley6811/TAIMAU/archive/master.zip")
-            with open("temp_github_repo.zip", "w") as wf:
-                wf.write(zipf.read())
-            wf.close()
+            src_url = "https://github.com/Ripley6811/TAIMAU/archive/master.zip"
+            zipr = urlopen(src_url)
+            zf = ZipFile(StringIO(zipr.content))
+            zf.extractall()
+
+            # Copy files from master to src
+            #XXX: From http://stackoverflow.com/questions/7419665/python-move-and-overwrite-files-and-folders
+            root_src_dir = os.path.join(os.getcwd(),"TAIMAU-master\\src")
+            root_dst_dir = os.path.join(os.getcwd(),"src")
+
+            for src_dir, dirs, files in os.walk(root_src_dir):
+                dst_dir = src_dir.replace(root_src_dir, root_dst_dir)
+                if not os.path.exists(dst_dir):
+                    os.mkdir(dst_dir)
+                for file_ in files:
+                    src_file = os.path.join(src_dir, file_)
+                    dst_file = os.path.join(dst_dir, file_)
+                    if os.path.exists(dst_file):
+                        os.remove(dst_file)
+                    shutil.move(src_file, dst_dir)
+
+            # Delete the temp repo folder.
+            shutil.rmtree(os.path.join(os.getcwd(),"TAIMAU-master"))
+
+            # Prompt user to restart program.
+            title = u'Restart required.'
+            message = u'Restart the program for latest version.'
+            tkMessageBox.showwarning(title, message)
